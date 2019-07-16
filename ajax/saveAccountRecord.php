432bbfb02e10330cb8ac9d1581d03dc671173b5a
R. A. G. Daniel <Rob.Daniel@uk.ibm.com>
2019-07-16 15:26:49 +0100

    Save Account Record

---
+++ ajax/saveAccountRecord.php
@@ -0,0 +1,41 @@
+<?php
+
+use itdq\DbTable;
+use itdq\FormClass;
+use upes\AccountRecord;
+use upes\AccountTable;
+use upes\AllTables;
+
+set_time_limit(0);
+ob_start();
+
+
+try {
+    $accountRecord = new AccountRecord();
+    $accountTable = new AccountTable(AllTables::$ACCOUNT);
+    $accountRecordData = array_map('trim', $_POST);
+
+    $accountRecordData['ACCOUNT_ID'] = $_POST['mode']==FormClass::$modeDEFINE ? null : $accountRecordData['ACCOUNT_ID'];
+
+    $accountRecord->setFromArray($accountRecordData);
+
+    $saveRecord = $_POST['mode']==FormClass::$modeDEFINE ? $accountTable->insert($accountRecord) : $accountTable->update($accountRecord);
+    $accountId  = $_POST['mode']==FormClass::$modeDEFINE ? $accountTable->lastId() : $accountRecordData['ACCOUNT_ID'];
+
+} catch (Exception $e) {
+    echo $e->getCode();
+    echo $e->getMessage();
+    print_r($e->getTrace());
+}
+
+$messages = ob_get_clean();
+$success = empty($messages);
+if($success){
+    $messages = " Account: " . $accountRecordData['ACCOUNT'] . "<br/>Account Id:" . $accountId . "<br/>";
+    $messages.= $_POST['mode']==FormClass::$modeDEFINE ? "Created" : "Updated" ;
+}
+
+$response = array('success'=>$success,'accountId' => $accountId, 'saveResponse' => $saveRecord, 'Messages'=>$messages);
+
+ob_clean();
+echo json_encode($response);
--- pa_manageAccounts.php
+++ pa_manageAccounts.php
@@ -14,6 +14,10 @@ Trace::pageOpening($_SERVER['PHP_SELF']);
 <?php
 $accountsRecord = new AccountRecord();
 $accountsRecord->displayForm(itdq\FormClass::$modeDEFINE);
+
+
+include_once 'includes/modalError.html';
+
 ?>
 </div>
 
@@ -25,6 +29,66 @@ $(document).ready(function(){
 	$('#accountsForm').submit(function(e){
 		console.log(e);
 		e.preventDefault();
+
+		var submitBtn = $(e.target).find('input[name="Submit"]').addClass('spinning');
+
+console.log(submitBtn);
+
+		var url = 'ajax/saveAccountRecord.php';
+
+		var disabledFields = $(':disabled');
+		$(disabledFields).removeAttr('disabled');
+		var formData = $("#accountsForm").serialize();
+
+		console.log(formData);
+
+		$(disabledFields).attr('disabled',true);
+		$.ajax({
+			type:'post',
+		  	url: url,
+		  	data:formData,
+		  	context: document.body,
+	      	success: function(response) {
+	      		var responseObj = JSON.parse(response);
+	      		if(responseObj.success){
+		    	    $(submitBtn).removeClass('spinning').attr('disabled',false);
+		    	    $('#accountsForm').trigger("reset");
+				} else {
+     	    	    $(submitBtn).removeClass('spinning').attr('disabled',false);
+		    	    $('#accountsForm').trigger("reset");
+	                $('.modal-body').html(responseObj.Messages);
+	                $('.modal-body').addClass('bg-danger');
+	                $('#modalError').modal('show');
+				}
+// 	    	    psRateTable.ajax.reload();
+          	},
+	      	fail: function(response){
+					console.log('Failed');
+					console.log(response);
+	                $('.modal-body').html("<h2>Json call to save record Failed.</h2><br>Tell Rob");
+	                $('.modal-body').addClass('bg-warning');
+	                $('#modalError').modal('show');
+	                $(submitBtn).removeClass('spinning').attr('disabled',false);
+				},
+	      	error: function(error){
+	            //	handle errors here. What errors	            :-)!
+	        		console.log('Ajax error' );
+	        		console.log(error.statusText);
+	                $('.modal-body').html("<h2>Json call to save record Error'd :<br/>" + error.statusText + "</h2>Tell Rob");
+	                $('.modal-body').addClass('bg-warning');
+	                $('#modalError').modal('show');
+	                $(submitBtn).removeClass('spinning').attr('disabled',false);
+	        	},
+	      	always: function(){
+	        		console.log('--- saved resource request ---');
+	      	}
+		});
+
+
+
+
+
+
 		});
 
 
