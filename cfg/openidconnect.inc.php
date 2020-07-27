28f9f1811545adf4c51cb5767ebb623aae633d43
Robert Daniel <rob.daniel@uk.ibm.com>
2020-07-27 09:21:20 +0100

    Add pre-prod SSO

--- cfg/openidconnect.inc.php
+++ cfg/openidconnect.inc.php
@@ -26,9 +26,9 @@ $config_openidconnect = new stdClass();
 $config_openidconnect->client_id['staging']      = !empty($_ENV['sso_staging_id']) ? $_ENV['sso_staging_id'] :null ;
 $config_openidconnect->client_secret['staging']  = !empty($_ENV['sso_staging_secret']) ?  $_ENV['sso_staging_secret'] :null ;
 
-$config_openidconnect->authorize_url['staging']  = "https://w3id.alpha.sso.ibm.com/isam/oidc/endpoint/amapp-runtime-oidcidp/authorize";
-$config_openidconnect->token_url['staging']      = "https://w3id.alpha.sso.ibm.com/isam/oidc/endpoint/amapp-runtime-oidcidp/token";
-$config_openidconnect->introspect_url['staging'] = "https://w3id.alpha.sso.ibm.com/isam/oidc/endpoint/amapp-runtime-oidcidp/introspect";
+$config_openidconnect->authorize_url['staging']  = "https://preprod.login.w3.ibm.com/oidc/endpoint/default/authorize";
+$config_openidconnect->token_url['staging']      = "https://preprod.login.w3.ibm.com/oidc/endpoint/default/token";
+$config_openidconnect->introspect_url['staging'] = "https://preprod.login.w3.ibm.com/oidc/endpoint/default/introspect";
 
 
 $config_openidconnect->client_id['production']      = !empty($_ENV['sso_production_id']) ? $_ENV['sso_production_id'] :null ;
