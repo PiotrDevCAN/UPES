18606a5a4563bee0a6e1dcab2b8498c4f0313aba
Robert Daniel <rob.daniel@uk.ibm.com>
2020-07-29 10:32:43 +0100

    Phpinfo

--- cdi_test.php
+++ cdi_test.php
@@ -1,24 +1,5 @@
 <?php
-use itdq\Trace;
-use itdq\AuditTable;
-use itdq\AuditRecord;
-use itdq\BlueMail;
-use upes\PesEmail;
 
-Trace::pageOpening($_SERVER['PHP_SELF']);
+phpinfo();
 
-
-$array1 = array('A'=>'aa','b'=>'b1','c'=>'c1');
-$array2 = array('A'=>'aa','b'=>'b2','c'=>'c2');
-
-$bigArray = array($array1,$array2);
-
-echo "<pre>";
-print_r($bigArray);
-echo "</pre>";
-
-
-
-
-
-Trace::pageLoadComplete($_SERVER['PHP_SELF']);
+print_r($GLOBALS);
