4d569c9dfe0fe3c998fe14bb4ea50d85d014f062
Robert Daniel <rob.daniel@uk.ibm.com>
2021-01-27 16:26:13 +0000

    More wording changes

--- emailAttachments/emailBodies/request_international.php
+++ emailAttachments/emailBodies/request_international.php
@@ -4,15 +4,17 @@ $emailBody = <<<endOfEmailBody
 <p>We believe you are about to engage on a UKI FSS account.  IBM are contractually obliged to ensure that anyone engaging on any FSS customer accounts are PES cleared.  To allow us to process this requirement, please return the below required documents to us at your earliest convenience.</p>
 <ul>
 <li>Fully completed Application Form(s) :<br/>&&name_of_application_form&&</li>
-<li>An email from your IBM Manager confirming your IBM Start Date <b>OR</b> a screen print from the relevant page of the 'Work Day' system.<br/>*<b>Further information and evidence</b> may be required *</li>
 <li>A Certified copy of the photo page of your Passport or ID Card along with your VISA/Work Permit if required</li>
 <li>If your ID card does not contain your current address, please send a certified copy of a utility bill (not mobile)/Bank statement (less than 3 months old) - as evidence of your current address</li>
+<li>An email from your IBM Manager confirming your IBM Start Date <b>OR</b> a screen print from the relevant page of the 'Work Day' system.<br/>*<b>Further information and evidence</b> may be required *</li>
 </ul>
-<p><b>The Certification MUST be done by another IBM'er</b>, to confirm that they have seen the original document.  The following statement should be <b>handwritten</b> on <b>each document</b>, on the <b>same side as the image</b>.</p>
+<p><b>The Certification MUST be done by an IBM'er<b>, to confirm that they have seen the original document. Either physically by another IBM’er or Virtually by your IBM Manager.<p>
+The following statement should be added on each document, on the same side as the image, or an email can be sent directly to the PES team by your certifier to confirm which documents have been viewed.
+.</p>
 <h4><span style='color:red'><center>True & Certified Copy<br/>Name of certifier in BLOCK CAPITALS<br/>IBM Serial number of certifier<br/>Certification Date<br/>Signature of certifier</center></span></h4>
 <p>If you have any questions, you do not have any of the listed documents or are unsure about the process please contact the PES Team.</p>
 <p><b>Please Note</b></p>
-<p>Due to the recent situation we understand that many people will be unable to meet with fellow IBM'ers to have their documents Certified.  We have implemented a  'provisional clearance' process and wil accept documents without certification, at this time.  However these documents will require to be certified as soon as the restrictions are lifted.</p>
+<p>If you are unable to get your documents certified (physically or virtually), we do have a provisional clearance process and will accept documents without certification - however these documents will need to be certified as soon as the restrictions are lifted or as soon as you can.</p>
 <p>This will not give you full PES clearance for the account, so if you can get them certified correctly (ie another IBM'er seeing the document and signing) please do so.</p>
 <p>Many Thanks for your cooperation,</p>
 <p>&&account_name&& &nbsp; PES Team</p>
