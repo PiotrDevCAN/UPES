2a8f80ae36fe6b7bd6c8397dead4ee5c1d25a9a8
Piotr <piotr.tajanowicz@ibm.com>
2021-08-20 11:16:26 +0200

    update in Provisional Clearance notification

--- upes/AccountPersonRecord.php
+++ upes/AccountPersonRecord.php
@@ -148,14 +148,15 @@ class AccountPersonRecord extends DbRecord
     private static $pesCancelPesEmailPattern = array('/&&candidate&&/','/&&upesref&&/','/&&requestor&&/','/&&account&&/');
 
     private static $pesClearedProvisionalEmail = 'Hello &&candidate&&,
-                                              <br/>Due to the recent situation we understand that many people will be unable to meet with fellow IBM\'ers to have their documents Certified.  We have implemented a \'provisional clearance\' process and will be accepting all documents without certification - however these documents will require to be certified as soon as the restrictions are lifted.
-                                              <br/>Therefore I can confirm that you have provisionally passed  &&accountName&& PES Screening.
+                                              <br/>I can confirm that you have provisionally cleared  &&accountName&& PES Screening.
                                               <br/><b>Please note that this will not give you full PES clearance, and your account may not recognise Provisional Clearance, therefore, if you can get your documents certified correctly (as per below) please do so.</b>
-                                              <br/>When sending your document please only send to the PES team.
-                                              <br/><b>The Certification MUST be done by another IBM�er</b>, to confirm that they have seen the original document. The following statement should be handwritten on <b>each document</b>, on the <b>same side as the image</b>.
-                                              <br/><span style=\'text-align:center;color:red\'>True & Certified Copy<br/>Name of certifier  in BLOCK CAPITALS<br/>IBM Serial number of certifier<br/>Certification Date<br/>Signature of certifier<br/></span>
+                                              <p><b>The Certification MUST be done by an IBM�er<b>, to confirm that they have seen the original document. Either physically by another IBM’er or Virtually (over webex) by your IBM Manager.<p> For Physical - The following statement should be added on each document, on the same side as the image.</p>
+                                              <h4><span style=\'text-align:center;color:red\'><center>True & Certified Copy<br/>Name of certifier in BLOCK CAPITALS<br/>IBM Serial number of certifier<br/>Certification Date<br/>Signature of certifier</center></span></h4>
+                                              <p>or Virtual - an email can be sent directly to the PES team by your certifier <b>to confirm which original documents have been viewed, over video conferencing, along with the date and time they were viewed.<b><p>
                                               <br/>If you need any more information regarding your PES clearance, please let me know.
+                                              <br/>When sending your document please only send to the PES team.
                                               <br/>Many Thanks for your cooperation,';
+
     private static $pesClearedProvisionalEmailPattern = array('/&&candidate&&/','/&&accountName&&/');
 
    //  public static $pesTaskId = array('lbgvetpr@uk.ibm.com'); // Only first entry will be used as the "contact" in the PES status emails.
