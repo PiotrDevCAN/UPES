1efd330b0a102a90683910930b7f90152faae3f6
Piotr Tajanowicz <piotr.tajanowicz@kyndryl.com>
2022-05-20 14:17:04 +0200

    PES Status API test

--- api/pesStatus.php
+++ api/pesStatus.php
@@ -2,11 +2,17 @@
 
 use upes\AllTables;
 
+function ob_html_compress($buf){
+    return str_replace(array("\n","\r"),'',$buf);
+}
+
 if($_REQUEST['token']!= $token) {
     header('Content-Type: application/json');
     echo json_encode(array('success'=>false,'data'=>array(),'messages'=>'Invalid Token'));    
     return;    
 }
+
+$noTrim = !empty($_REQUEST['noTrim']) ? true : false;
 
 $sql = " SELECT AP.ACCOUNT_ID, P.CNUM, P.EMAIL_ADDRESS, PROCESSING_STATUS, PROCESSING_STATUS_CHANGED ";
 $sql.= ", PES_DATE_REQUESTED, PES_REQUESTOR, PES_DATE_RESPONDED, PES_STATUS_DETAILS, PES_STATUS";
@@ -29,19 +35,34 @@ if (! $rs) {
 $endExec = microtime(true);
 $timeMeasurements['db_exec'] = (float)($endExec-$start);
 
+$startDataTrim = microtime(true);
+
 $data = array();
 $count = 1;
-while(($row=db2_fetch_assoc($rs))==true){
-    $row = array_map('trim',$row);
+// while(($row=db2_fetch_assoc($rs))==true){
+while(($row=db2_fetch_assoc($rs))){
+    if ($noTrim === false) {
+        $row = array_map('trim',$row);
+    }
     $data[] = $row;
+    $count++;
 }
 
 $endDataTrim = microtime(true);
-$timeMeasurements['data_trim'] = (float)($endDataTrim-$start);
+$timeMeasurements['data_trim'] = (float)($endDataTrim-$startDataTrim);
 
 $messages = ob_get_clean();
-
 $success = empty($messages);
+
+if (isset($_SERVER['HTTP_ACCEPT_ENCODING'])) {
+    if (substr_count($_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip')) {
+        ob_start("ob_gzhandler");
+    } else {
+        ob_start("ob_html_compress");
+    }
+} else {
+    ob_start("ob_html_compress");
+}
 
 header('Content-Type: application/json');
 echo json_encode(array(
