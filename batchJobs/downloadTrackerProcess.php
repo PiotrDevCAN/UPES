ad15a5bcf5aff42a4c072ea986528417aee5097c
Piotr Tajanowicz <piotr.tajanowicz@kyndryl.com>
2022-06-22 11:36:38 +0200

    Tracker extract v2

--- batchJobs/downloadTrackerProcess.php
+++ batchJobs/downloadTrackerProcess.php
@@ -12,10 +12,10 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-// require_once("php/errorHandlers.php");
+require_once("php/errorHandlers.php");
 
-// set_error_handler('myErrorHandler');
-// register_shutdown_function('fatalErrorShutdownHandler');
+set_error_handler('myErrorHandler');
+register_shutdown_function('fatalErrorShutdownHandler');
 
 error_log("download tracker process started");
 
@@ -70,29 +70,30 @@ if (isset($argv[1])) {
     $pesTrackerTable = new AccountPersonTable(AllTables::$ACCOUNT_PERSON);
 
     try {
-        // $pesTrackerTable->getTracker($type, $spreadsheet);
-        // // Set active sheet index to the first sheet, so Excel opens this as the first sheet
-        // $spreadsheet->setActiveSheetIndex(0);
-        // // Redirect output to a client�s web browser (Xlsx)
-        // DbTable::autoSizeColumns($spreadsheet);
-        // $fileNameSuffix = $now->format('Ymd_His');
-        // $fileNamePart = $title . $fileNameSuffix . '.xlsx';
+        $pesTrackerTable->getTracker($type, $spreadsheet);
+        // Set active sheet index to the first sheet, so Excel opens this as the first sheet
+        $spreadsheet->setActiveSheetIndex(0);
+        // Redirect output to a client�s web browser (Xlsx)
+        DbTable::autoSizeColumns($spreadsheet);
+        $fileNameSuffix = $now->format('Ymd_His');
+        $fileNamePart = $title . $fileNameSuffix . '.xlsx';
         // $fileName = '../extracts/'.$fileNamePart;
+        $fileName = './extracts/'.$fileNamePart;
 
-        // // ob_clean();
-        // // header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-        // // header('Content-Disposition: attachment;filename="PES_Tracker_' . $fileNameSuffix . '.xlsx"');
-        // // header('Cache-Control: max-age=0');
-        // // // If you're serving to IE 9, then the following may be needed
-        // // header('Cache-Control: max-age=1');
-        // // // If you're serving to IE over SSL, then the following may be needed
-        // // header('Expires: Mon, 26 Jul 1997 05:00:00 GMT'); // Date in the past
-        // // header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT'); // always modified
-        // // header('Cache-Control: cache, must-revalidate'); // HTTP/1.1
-        // // header('Pragma: public'); // HTTP/1.0
-        // $writer = IOFactory::createWriter($spreadsheet, 'Xlsx');
-        // // $writer->save('php://output');
-        // $writer->save($fileName);
+        // ob_clean();
+        // header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+        // header('Content-Disposition: attachment;filename="PES_Tracker_' . $fileNameSuffix . '.xlsx"');
+        // header('Cache-Control: max-age=0');
+        // // If you're serving to IE 9, then the following may be needed
+        // header('Cache-Control: max-age=1');
+        // // If you're serving to IE over SSL, then the following may be needed
+        // header('Expires: Mon, 26 Jul 1997 05:00:00 GMT'); // Date in the past
+        // header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT'); // always modified
+        // header('Cache-Control: cache, must-revalidate'); // HTTP/1.1
+        // header('Pragma: public'); // HTTP/1.0
+        $writer = IOFactory::createWriter($spreadsheet, 'Xlsx');
+        // $writer->save('php://output');
+        $writer->save($fileName);
 
         // $excelOutput = ob_get_clean();
 
@@ -114,18 +115,18 @@ if (isset($argv[1])) {
 
         $pesTaskid = 'atm.pes.processing@uk.ibm.com';
 
-        $pesAttachments = array();
-        // $handle = fopen($fileName, "r", true);
-        // $applicationForm = fread($handle, filesize($fileName));
-        // fclose($handle);
-        // $encodedAttachmentFile = base64_encode($applicationForm);
+        $handle = fopen($fileName, "r", true);
+        $applicationForm = fread($handle, filesize($fileName));
+        fclose($handle);
+        $encodedAttachmentFile = base64_encode($applicationForm);
 
-        // $pesAttachments[] = array(
-        //     'filename'=>$title . $fileNameSuffix . '.xlsx',
-        //     'content_type'=>'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-        //     'data'=>$encodedAttachmentFile,
-        //     'path'=>$fileName
-        // );
+        $pesAttachments = array();
+        $pesAttachments[] = array(
+            'filename'=>$title . $fileNameSuffix . '.xlsx',
+            'content_type'=>'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+            'data'=>$encodedAttachmentFile,
+            'path'=>$fileName
+        );
 
         $sendResponse = BlueMail::send_mail($toEmail, $subject, $emailBody, $pesTaskid, array(), array(), false, $pesAttachments);
 
--- php/siteheader.php
+++ php/siteheader.php
@@ -8,6 +8,7 @@ use ByJG\Session\JwtSession;
 use itdq\BluePages;
 use itdq\BluePagesSLAPHAPI;
 use itdq\JwtSecureSession;
+use PhpOffice\PhpSpreadsheet\Helper\Sample;
 
 function ob_html_compress($buf){
     return str_replace(array("\n","\r"),'',$buf);
@@ -138,28 +139,35 @@ function do_error($page = array())
 
 function do_auth($group = null)
 {
-
-if(stripos($_ENV['environment'], 'dev')) {
-    $_SESSION['ssoEmail'] = $_ENV['SERVER_ADMIN'];
-} else {
-    include_once "class/include.php";
-    $auth = new Auth();
-    if(!$auth->ensureAuthorized()){
-        die('Invalid logon attempt');
+    if(stripos($_ENV['environment'], 'dev')) {
+        $_SESSION['ssoEmail'] = $_ENV['SERVER_ADMIN'];
     } else {
-        $_SESSION['ssoEmail'] = $_SESSION['ssoEmail'];
-        if(isset($_SESSION['somethingChanged']))
-            {
-            echo "<br/><br/><span style='font-weight:bold;'>Warning: </span> The values that are returned from w3ID/IBMID has probably been changed.<br/><br/>No need to panic, this is very easy to fix.<br/>This is your session currently:<br/><br/><code>";
-            var_dump($_SESSION);
-            echo "</code><br/><br/>" . "Everything you see there except <span style='font-weight:bold;'>somethingChanged</span> is coming from w3ID/IBMID service.";
-            echo '<br/>You now need to look into <span style="font-weight:bold;">private function processOpenIDConnectCallback($data)</span> in <span style="font-weight:bold;">class/auth.class.php</span> and read the comments.';
-            echo "<br/>Please keep in mind, that even if sign in is technically working now, you should not use the code in production without strict checking of those values.";
-            echo "<br/><br/>";
-            echo "What you can do now is:";
-            echo "<br/>a) Paste this warning message to <a href='https://github.ibm.com/CWT/auth-openidconnect-w3/issues/' target='_blank'>GitHub Issues</a> and wait for it to be fixed.";
-            echo '<br/>b) Very easily adjust the code in private function processOpenIDConnectCallback($data) with new and correct values and <a href="https://github.ibm.com/CWT/auth-openidconnect-w3/issues/" target="_blank">open a new issue</a> or <a href="https://github.ibm.com/CWT/auth-openidconnect-w3/pulls" target="_blank">create a new pull request</a>.';
-            echo '<br/><br/>Note: When trying to fix this yourself, do remember to always clear cookies when refreshing the page.';
+
+        // batchJobs
+        $helper = new Sample();
+        if ($helper->isCli()) {
+            $helper->log('This example should only be run from a Web Browser' . PHP_EOL);
+            $_SESSION['ssoEmail'] = $_ENV['SERVER_ADMIN'];
+        } else {
+            include_once "class/include.php";
+            $auth = new Auth();
+            if(!$auth->ensureAuthorized()){
+                die('Invalid logon attempt');
+            } else {
+                $_SESSION['ssoEmail'] = $_SESSION['ssoEmail'];
+                if(isset($_SESSION['somethingChanged']))
+                {
+                    echo "<br/><br/><span style='font-weight:bold;'>Warning: </span> The values that are returned from w3ID/IBMID has probably been changed.<br/><br/>No need to panic, this is very easy to fix.<br/>This is your session currently:<br/><br/><code>";
+                    var_dump($_SESSION);
+                    echo "</code><br/><br/>" . "Everything you see there except <span style='font-weight:bold;'>somethingChanged</span> is coming from w3ID/IBMID service.";
+                    echo '<br/>You now need to look into <span style="font-weight:bold;">private function processOpenIDConnectCallback($data)</span> in <span style="font-weight:bold;">class/auth.class.php</span> and read the comments.';
+                    echo "<br/>Please keep in mind, that even if sign in is technically working now, you should not use the code in production without strict checking of those values.";
+                    echo "<br/><br/>";
+                    echo "What you can do now is:";
+                    echo "<br/>a) Paste this warning message to <a href='https://github.ibm.com/CWT/auth-openidconnect-w3/issues/' target='_blank'>GitHub Issues</a> and wait for it to be fixed.";
+                    echo '<br/>b) Very easily adjust the code in private function processOpenIDConnectCallback($data) with new and correct values and <a href="https://github.ibm.com/CWT/auth-openidconnect-w3/issues/" target="_blank">open a new issue</a> or <a href="https://github.ibm.com/CWT/auth-openidconnect-w3/pulls" target="_blank">create a new pull request</a>.';
+                    echo '<br/><br/>Note: When trying to fix this yourself, do remember to always clear cookies when refreshing the page.';
+                }
             }
         }
     }
@@ -314,6 +322,7 @@ if (isset($_SERVER['HTTP_ACCEPT_ENCODING'])) {
     // exit('ob_html_compress 2');
 }
 
+$_ENV['environment'] = 'UPES_UT';
 $GLOBALS['Db2Schema'] = strtoupper($_ENV['environment']);
 $https = (isset($_SERVER['HTTPS']) && strtolower($_SERVER['HTTPS']) == "on");
 
